f418f7bfe74e659c6838a397bcf58b2d5e614220
JonasFortalezaTec <jonas@fortalezatec.com.br>
2025-04-17 15:37:21 -0300

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <div>
       <main>
-        <h1>Página HOME</h1>
-        <span>Usando API Routes</span>
+        <h1>HOME</h1>
+        <span>API Routes</span>
       </main>
     </div>
   );
